4ec7769c2b1eac396d0386a7334f1eb350e1bbb6
Long <18130132@st.hcmuaf.edu.vn>
2020-05-29 22:23:33 +0700

    Update Moving Code

---
+++ InsideBook/Assets/2_Scripts/CharacterMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private MoveObject moveObject;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    public void PlayerCharacterMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //moveObject.Moving();
+        }
+    }
+}
+public class SaveKey
+{
+
+}
+
+
--- InsideBook/Assets/2_Scripts/MoveObject.cs
+++ InsideBook/Assets/2_Scripts/MoveObject.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D _Object;
+    public GameObject gameObject;
+    public float speed = 1f;
+    public float begin, end;
+    public bool isMainCharacter = false;
     void Start()
     {
-        
+        _Object.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isMainCharacter)
+        {
+           // Moving();
+        }
+    }
+    public void ObjectMoving() //object preriod moving
+    {
+        if(gameObject.transform.position.x == begin)
+        {
+            MovingRight();
+            
+        }
+
+    }
+    public void MovingRight() {
+        _Object.velocity = Vector2.right * speed * Time.deltaTime;
+    }
+    public void MovingLeft()
+    {
+        _Object.velocity = Vector2.left * speed * Time.deltaTime;
     }
 }
