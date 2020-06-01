cbcec7f36ef6e431e63f5095da2fa0835d855e3e
p.thieenlong <18130132@st.hcmuaf.edu.vn>
2020-06-01 17:42:51 +0700

    Update moving script 060120

--- InsideBook/Assets/2_Scripts/CharacterMovement.cs
+++ InsideBook/Assets/2_Scripts/CharacterMovement.cs
@@ -5,26 +5,55 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    private MoveObject moveObject;
+    public Rigidbody2D rb;
+    //public Animator playerAnim;
+    public float speed = 300f;
+    public float jumpForce = 10f;
+    public bool isJump = false;
     void Start()
     {
-
+        rb.GetComponents<Rigidbody2D>();
     }
     void Update()
     {
-
-    }
+        PlayerCharacterMovement();
+        //Input.Get
+        // if(Input.GetKeyDown(KeyCode.A)){
+        //     //Debug.Log("a");
+        //     playerAnim.Play(SaveKey.Left);
+        //     rb.velocity = Vector2.left * speed * Time.deltaTime;
+        // }
+        // if(Input.GetKeyDown(KeyCode.D)){
+        //     //Debug.Log("d");
+        //     playerAnim.Play(SaveKey.Right);
+        //     rb.velocity = Vector2.right * speed * Time.deltaTime;
+        // }
+    }   
     public void PlayerCharacterMovement()
     {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            //moveObject.Moving();
+        if(Input.GetKeyDown(KeyCode.A)){
+            Debug.Log("a");
+            //playerAnim.Play(SaveKey.Left);
+            
+            rb.velocity = Vector2.left * speed * Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            Debug.Log("d");
+            //playerAnim.Play(SaveKey.Right);
+            rb.velocity = Vector2.right * speed * Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("space");
+            rb.velocity = new Vector2(0, jumpForce);
+            
         }
     }
 }
 public class SaveKey
 {
-
+    public const String Left = "LeftTrigger";
+    public const String Right = "RightTrigger";
+    
 }
 
 
--- InsideBook/Assets/2_Scripts/MoveObject.cs
+++ InsideBook/Assets/2_Scripts/MoveObject.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class MoveObject : MonoBehaviour
 {
     public Rigidbody2D _Object;
-    public GameObject gameObject;
-    public float speed = 1f;
-    public float begin, end;
-    public bool isMainCharacter = false;
+    public float speed = 20f;
+    //public float begin, end;
+    //public bool isMainCharacter = false;
     void Start()
     {
         _Object.GetComponent<Rigidbody>();
@@ -16,19 +15,23 @@ public class MoveObject : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (isMainCharacter)
-        {
-           // Moving();
-        }
+        //if (isMainCharacter)
+        
+            // MovingLeft();
+            // if(Input.GetKeyDown(KeyCode.A)){
+            //     Debug.Log("a");
+            // }   
     }
     public void ObjectMoving() //object preriod moving
-    {
-        if(gameObject.transform.position.x == begin)
-        {
-            MovingRight();
-            
-        }
-
+    { 
+        
+        // if(gameObject.transform.position.x = begin)
+        // {
+        //    MovingRight();
+        // }
+        // if(gameObject.transform.position.x = end) {
+        //    MovingLeft();
+        // }
     }
     public void MovingRight() {
         _Object.velocity = Vector2.right * speed * Time.deltaTime;
