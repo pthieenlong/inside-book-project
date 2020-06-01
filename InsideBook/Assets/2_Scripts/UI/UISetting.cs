3ee998663fc0fb6eada80f5587a50f9ce184a731
anhhinhsu <anhhinhsu@gmail.com>
2020-06-01 18:41:20 +0700

    add UI Setting, Lily spine

---
+++ InsideBook/Assets/2_Scripts/UI/UISetting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISetting : MonoBehaviour
+{
+    public void HideUI()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void ShowUI()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
