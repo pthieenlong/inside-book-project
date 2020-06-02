42255f30090dc9e7e906d9e9dcd894a86cbe6cb0
anhhinhsu <anhhinhsu@gmail.com>
2020-06-02 18:21:25 +0700

    fixing moving

--- InsideBook/Assets/2_Scripts/CharacterMovement.cs
+++ InsideBook/Assets/2_Scripts/CharacterMovement.cs
@@ -10,6 +10,7 @@ public class CharacterMovement : MonoBehaviour
     public float speed = 300f;
     public float jumpForce = 10f;
     public bool isJump = false;
+    float moveDirection = 0;
     void Start()
     {
         rb.GetComponents<Rigidbody2D>();
@@ -28,32 +29,53 @@ public class CharacterMovement : MonoBehaviour
         //     playerAnim.Play(SaveKey.Right);
         //     rb.velocity = Vector2.right * speed * Time.deltaTime;
         // }
-    }   
+    }
     public void PlayerCharacterMovement()
     {
-        if(Input.GetKeyDown(KeyCode.A)){
+        #region Horizontal
+        // if(Input.GetKeyDown(KeyCode.A)){
+        if (Input.GetKey(KeyCode.A))
+        {
             Debug.Log("a");
+            moveDirection = -1;
             //playerAnim.Play(SaveKey.Left);
-            
-            rb.velocity = Vector2.left * speed * Time.deltaTime;
+            // rb.velocity = Vector2.left * speed * Time.deltaTime;
         }
-        if(Input.GetKeyDown(KeyCode.D)){
+        // if(Input.GetKeyDown(KeyCode.D)){
+        if (Input.GetKey(KeyCode.D))
+        {
             Debug.Log("d");
+            moveDirection = 1;
             //playerAnim.Play(SaveKey.Right);
-            rb.velocity = Vector2.right * speed * Time.deltaTime;
+            // rb.velocity = Vector2.right * speed * Time.deltaTime;
         }
-        if(Input.GetKeyDown(KeyCode.Space)){
+
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            moveDirection = 0;
+        }
+
+        rb.velocity = rb.velocity.V2SetX(moveDirection * speed * Time.deltaTime);
+        #endregion Horizontal
+
+        #region Vertical
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
             Debug.Log("space");
-            rb.velocity = new Vector2(0, jumpForce);
-            
+            // rb.velocity = new Vector2(0, jumpForce);
+            // if (rb.IsTouchingLayers(LayerMask.NameToLayer("Ground"))) // chua dung, check tam thoi
+            // {
+            rb.velocity = rb.velocity.V2SetY(jumpForce);
+            // }
         }
+        #endregion Vertical
     }
 }
 public class SaveKey
 {
     public const String Left = "LeftTrigger";
     public const String Right = "RightTrigger";
-    
+
 }
 
 
---
+++ InsideBook/Assets/2_Scripts/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+    #region Vector Extensions
+    public static void V3SetX(this Vector3 v, float x)
+    {
+        Vector3 tempV = v;
+        tempV.x = x;
+        v = tempV;
+    }
+    public static void V3SetY(this Vector3 v, float y)
+    {
+        Vector3 tempV = v;
+        tempV.y = y;
+        v = tempV;
+    }
+
+    public static Vector2 V2SetX(this Vector2 v, float x)
+    {
+        Vector2 tempV = v;
+        tempV.x = x;
+        v = tempV;
+        return v;
+    }
+    public static Vector2 V2SetY(this Vector2 v, float y)
+    {
+        Vector2 tempV = v;
+        tempV.y = y;
+        v = tempV;
+        return v;
+    }
+    #endregion Vector Extensions
+}
